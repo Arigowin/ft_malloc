0fdfb61299006c587653686f00548d1efbb53380
Arigowin <dolewski@student.42.fr>
2018-05-08 19:40:53 +0200

    debug

--- include/malloc.h
+++ include/malloc.h
@@ -8,7 +8,9 @@
 
 typedef struct		s_node
 {
-	struct s_node	*end;
+	size_t			size;
+	char			is_free;
+	char			data[1];
 	struct s_node	*next;
 }					t_node;
 
@@ -24,7 +26,7 @@ typedef struct		s_lst
 static t_lst		*g_lst = NULL;
 
 void				*malloc(size_t size);
-void				free(void *ptr);
+// void				free(void *ptr);
 // void				*realloc(void *ptr, size_t size);
 // void				show_alloc_mem();
 t_lst				*get_lst(void);
